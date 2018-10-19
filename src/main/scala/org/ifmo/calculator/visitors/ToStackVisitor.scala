b11bbbe641a008578f682addf5b7d960613921cc
aleksZubakov <aleks.zubakov@gmail.com>
2018-10-19 22:21:56 +0300

    ToStackVisitor minor refactoring

--- src/main/scala/org/ifmo/calculator/visitors/ToStackVisitor.scala
+++ src/main/scala/org/ifmo/calculator/visitors/ToStackVisitor.scala
@@ -4,14 +4,10 @@ import org.ifmo.calculator.stack.elements._
 import org.ifmo.grammar.{NotFunAtAllParser, NotFunAtAllParserBaseVisitor}
 
 class ToStackVisitor extends NotFunAtAllParserBaseVisitor[List[StackElement]] {
-  val operationStack: List[StackElement] = List()
-
-
   override def visitLogicalCompareExpr(ctx: NotFunAtAllParser.LogicalCompareExprContext): List[StackElement] = {
     Operation.parseComparison(ctx.op.getText).asInstanceOf[StackElement] ::
       (ctx.left.accept(this) ++ ctx.right.accept(this))
   }
-
 
   override def visitLogicalBinaryExpr(ctx: NotFunAtAllParser.LogicalBinaryExprContext): List[StackElement] = {
     Operation.parseBoolean(ctx.op.getText) ::
@@ -21,7 +17,6 @@ class ToStackVisitor extends NotFunAtAllParserBaseVisitor[List[StackElement]] {
   override def visitLogicalParensExpr(ctx: NotFunAtAllParser.LogicalParensExprContext): List[StackElement] = {
     ctx.logical_expr().accept(this)
   }
-
 
   override def visitLogicalAtomExpr(ctx: NotFunAtAllParser.LogicalAtomExprContext): List[StackElement] = {
     List(BoolValue(ctx.bool_literal().getText.toBoolean))
@@ -51,14 +46,10 @@ class ToStackVisitor extends NotFunAtAllParserBaseVisitor[List[StackElement]] {
 
   override def visitBinary_expr(ctx: NotFunAtAllParser.Binary_exprContext): List[StackElement] = {
     if (ctx.arithm_expr() != null) {
-      return ctx.arithm_expr().accept(this)
+      ctx.arithm_expr().accept(this)
+    } else {
+      ctx.logical_expr().accept(this)
     }
-
-    if (ctx.logical_expr() != null) {
-      return ctx.logical_expr().accept(this)
-    }
-
-    return List()
   }
 }
 
