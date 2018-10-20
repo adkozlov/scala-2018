4f1bf1f5ca60b2345ee8a7d3e83ba3421d199b15
Roman Golyshev <anstreth@gmail.com>
2018-10-21 01:32:46 +0300

    refactor ArithmeticEvalVisitor

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,44 +8,41 @@ sealed trait EvalResult
 
 case class NumberResult(result: Double) extends EvalResult
 
-case class BooleanResult(result: Boolean) extends EvalResult
+object ArithmeticEvalVisitor extends ArithmeticBaseVisitor[NumberResult] {
+  private type DoubleOperator = (Double, Double) => Double
 
-object EvalVisitor extends ArithmeticBaseVisitor[EvalResult] {
-  override def visitExpression(ctx: ArithmeticParser.ExpressionContext): EvalResult = {
+  override def visitExpression(ctx: ArithmeticParser.ExpressionContext): NumberResult = {
     val operations = ctx.ops.asScala.map(parseOperand)
     val terms = ctx.rest.asScala.map {
       _.accept(this)
     }
 
-    operations
-      .zip(terms)
-      .foldLeft(ctx.head.accept(this)) { (acc, data) =>
-        val (op, cur) = data
-        (acc, cur) match {
-          case (NumberResult(l), NumberResult(r)) => NumberResult(op(l, r))
-          case _ => throw new IllegalArgumentException(s"Cannot perform operation on $acc and $op")
-        }
-      }
+    foldEquations(ctx.head.accept(this), operations, terms)
   }
 
-  override def visitTerm(ctx: ArithmeticParser.TermContext): EvalResult = {
+  override def visitTerm(ctx: ArithmeticParser.TermContext): NumberResult = {
     val operations = ctx.ops.asScala.map(parseOperand)
     val terms = ctx.rest.asScala.map {
       _.accept(this)
     }
 
-    operations
-      .zip(terms)
-      .foldLeft(ctx.head.accept(this)) { (acc, data) =>
-        val (op, cur) = data
-        (acc, cur) match {
-          case (NumberResult(l), NumberResult(r)) => NumberResult(op(l, r))
-          case _ => throw new IllegalArgumentException(s"Cannot perform operation on $acc and $op")
-        }
-      }
+    foldEquations(ctx.head.accept(this), operations, terms)
   }
 
-  private def parseOperand(token: Token): (Double, Double) => Double =
+  override def visitSignedAtom(ctx: ArithmeticParser.SignedAtomContext): NumberResult =
+    if (ctx.MINUS == null) {
+      ctx.atom().accept(this)
+    } else {
+      NumberResult(ctx.atom().accept(this).result)
+    }
+
+  override def visitNumberAtom(ctx: ArithmeticParser.NumberAtomContext): NumberResult =
+    NumberResult(ctx.NUMBER().getText.toDouble)
+
+  override def visitAtomInParens(ctx: ArithmeticParser.AtomInParensContext): NumberResult =
+    ctx.expression().accept(this)
+
+  private def parseOperand(token: Token): DoubleOperator =
     token.getType match {
       case ArithmeticParser.PLUS => _ + _
       case ArithmeticParser.MINUS => _ - _
@@ -54,21 +51,14 @@ object EvalVisitor extends ArithmeticBaseVisitor[EvalResult] {
       case _ => throw new IllegalArgumentException(s"Illegal operator $token")
     }
 
-  override def visitSignedAtom(ctx: ArithmeticParser.SignedAtomContext): EvalResult =
-    if (ctx.MINUS == null) {
-      ctx.atom().accept(this)
-    } else {
-      ctx.atom().accept(this) match {
-        case NumberResult(result) => NumberResult(-result)
-        case _ => throw new IllegalArgumentException(s"Cannot negate ${ctx.atom()}")
-      }
+  private def foldEquations(head: NumberResult,
+                            operations: Iterable[DoubleOperator],
+                            rest: Iterable[NumberResult]): NumberResult = {
+    operations.zip(rest).foldLeft(head) { (acc, data) =>
+      val (op, cur) = data
+      NumberResult(op(acc.result, cur.result))
     }
-
-  override def visitNumberAtom(ctx: ArithmeticParser.NumberAtomContext): EvalResult =
-    NumberResult(ctx.NUMBER().getText.toDouble)
-
-  override def visitAtomInParens(ctx: ArithmeticParser.AtomInParensContext): EvalResult =
-    ctx.expression().accept(this)
+  }
 }
 
 object Main {
@@ -76,7 +66,7 @@ object Main {
     val lexer = new ArithmeticLexer(CharStreams.fromString(s))
     val parser = new ArithmeticParser(new CommonTokenStream(lexer))
 
-    parser.expression().accept(EvalVisitor)
+    parser.expression().accept(ArithmeticEvalVisitor)
   }
 
   def main(args: Array[String]): Unit = {
