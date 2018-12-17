816be253dd10c749491b1a11dc74e190e7a81646
Alexandr Fedorov <fedorov2602@mail.ru>
2018-12-17 11:38:46 +0300

    HList from lecture

---
+++ src/main/scala/ru/hse/scala/hlist/HList.scala
@@ -0,0 +1,27 @@
+package ru.hse.scala.hlist
+
+sealed trait HList
+
+object HList {
+
+  case class HCons[+H, +T <: HList](head: H, tail: T) extends HList
+
+  case object HNil extends HList
+
+  implicit class HListExt[R <: HList](private val list: R) extends AnyVal {
+
+    def ::[H](head: H) = HCons(head, list)
+
+    def :::[L <: HList, Result <: HList](left: L)
+                                        (implicit appendable: Appendable[L, R, Result]): Result =
+      appendable(left, list)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val list = ("hello" :: 42 :: false :: HNil) ::: ("world" :: HNil)
+
+    val hello: String = list.head
+    val world: String = list.tail.tail.tail.head
+    println(s"$hello $world")
+  }
+}
