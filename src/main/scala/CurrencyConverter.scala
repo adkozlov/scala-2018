f304414388db0790cb0e34f262689f5c8a51bbd8
andrey <andrey.smirdin@gmail.com>
2018-12-18 20:27:36 +0300

    Currency converter fixed

--- src/main/scala/CurrencyConverter.scala
+++ src/main/scala/CurrencyConverter.scala
@@ -10,15 +10,23 @@ object CurrencyConverter {
   // I have to request information for previous days too.
   private val DAYS_TO_LOOK_BACK = 10
 
-  sealed trait Currency
-
   class ConversionException(string: String) extends Exception(string)
 
-  object RUR extends Currency
+  sealed trait Currency {
+    val API_SHORT_NAME: String
+  }
 
-  object USD extends Currency
+  object RUR extends Currency {
+    override val API_SHORT_NAME = "RUB"
+  }
 
-  object EUR extends Currency
+  object USD extends Currency {
+    override val API_SHORT_NAME: String = "USD"
+  }
+
+  object EUR extends Currency {
+    override val API_SHORT_NAME: String = "EUR"
+  }
 
   implicit class Convertable(private val amount: Double) {
     def RUR: ConversionFrom = new ConversionFrom(CurrencyConverter.RUR)
@@ -28,12 +36,12 @@ object CurrencyConverter {
     def EUR: ConversionFrom = new ConversionFrom(CurrencyConverter.EUR)
 
     class ConversionFrom(val from: Currency) {
-      def to(currency: Currency): Convertation = new Convertation(currency)
+      def to(currency: Currency): Conversion = new Conversion(currency)
 
-      class Convertation(to: Currency) {
-        def at(day: Int): ConvertationAtDay = new ConvertationAtDay(day)
+      class Conversion(to: Currency) {
+        def at(day: Int): ConversionAtDay = new ConversionAtDay(day)
 
-        class ConvertationAtDay(val day: Int) {
+        class ConversionAtDay(val day: Int) {
           def january(year: Int): Double = convert(year, 1)
 
           def february(year: Int): Double = convert(year, 2)
@@ -48,28 +56,26 @@ object CurrencyConverter {
 
           def july(year: Int): Double = convert(year, 7)
 
+          def august(year: Int): Double = convert(year, 8)
+
+          def september(year: Int): Double = convert(year, 9)
+
+          def october(year: Int): Double = convert(year, 10)
+
           private def convert(year: Int, month: Int): Double = {
             val date = LocalDateTime.of(year, month, day, 0, 0)
             val formatter = DateTimeFormatter.ofPattern("YYYY-MM-dd")
             val exchangeRates = scala.io.Source.fromURL(
               s"""$API_URL
-                 |?start_at=${date.minusDays(10).format(formatter)}
+                 |?start_at=${date.minusDays(DAYS_TO_LOOK_BACK).format(formatter)}
                  |&end_at=${date.format(formatter)}
                  |&base=$BASE""".stripMargin.replaceAll("\n", "")
             ).mkString
-            println(exchangeRates)
             amount / getExchangeRate(from, exchangeRates) * getExchangeRate(to, exchangeRates)
           }
 
           private def getExchangeRate(currency: Currency, data: String): Double = {
-            val SHORT_FOR_RUBLE = "RUB"
-            val SHORT_FOR_EURO = "EUR"
-            val SHORT_FOR_USD = "USD"
-            val shortForCurrency = currency match {
-              case CurrencyConverter.RUR => SHORT_FOR_RUBLE
-              case CurrencyConverter.EUR => SHORT_FOR_EURO
-              case CurrencyConverter.USD => SHORT_FOR_USD
-            }
+            val shortForCurrency = currency.API_SHORT_NAME
             val currencyRegex = s"""(?<=$shortForCurrency":)(.*?)(?=,)""".r
             currencyRegex findFirstIn data match {
               case Some(v) => v.toDouble
@@ -77,15 +83,11 @@ object CurrencyConverter {
             }
           }
 
-          def august(year: Int): Double = convert(year, 8)
-
-          def september(year: Int): Double = convert(year, 9)
-
-          def october(year: Int): Double = convert(year, 10)
-
           def november(year: Int): Double = convert(year, 11)
 
           def december(year: Int): Double = convert(year, 12)
+
+
         }
 
       }
--- src/test/scala/TestCurrencyConverter.scala
+++ src/test/scala/TestCurrencyConverter.scala
@@ -5,5 +5,7 @@ class TestCurrencyConverter extends FlatSpec with Matchers {
 
   "CurrencyConverter" should " converted correctly" in {
     ((100 USD) to RUR at 10 february 2012) should be(2996.03 +- 0.01)
+    ((1000 RUR) to EUR at 22 march 2018) should be(14.11 +- 0.01)
+    ((20 EUR) to USD at 14 february 2010) should be(27.35 +- 0.01)
   }
 }
