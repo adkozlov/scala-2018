88cdc6f46ad1835b369d41ea73c07e3d697c2043
andrey <andrey.smirdin@gmail.com>
2018-12-18 18:19:11 +0300

    Length converter.

---
+++ src/main/scala/LengthConverter.scala
@@ -0,0 +1,41 @@
+object LengthConverter {
+
+  val M_TO_KM = 0.001
+  val M_TO_MI = 0.000621371
+  val M_TO_FT = 3.28084
+  val M_TO_IN = 39.3701
+  val M_TO_YD = 1.09361
+
+  sealed trait Length
+
+  class RatioWithMeter(private val amount: Double) {
+    def m: Double = amount
+
+    def km: Double = amount * M_TO_KM
+
+    def mi: Double = amount * M_TO_MI
+
+    def ft: Double = amount * M_TO_FT
+
+    def in: Double = amount * M_TO_IN
+
+    def yd: Double = amount * M_TO_YD
+  }
+
+  object to
+
+  implicit class Convertable(private val amount: Double) extends AnyVal {
+    def m(o: to.type): RatioWithMeter = new RatioWithMeter(amount)
+
+    def km(o: to.type): RatioWithMeter = new RatioWithMeter(amount / M_TO_KM)
+
+    def mi(o: to.type): RatioWithMeter = new RatioWithMeter(amount / M_TO_MI)
+
+    def ft(o: to.type): RatioWithMeter = new RatioWithMeter(amount / M_TO_FT)
+
+    def in(o: to.type): RatioWithMeter = new RatioWithMeter(amount / M_TO_IN)
+
+    def yd(o: to.type): RatioWithMeter = new RatioWithMeter(amount / M_TO_YD)
+  }
+
+}
---
+++ src/test/scala/TestLengthConverter.scala
@@ -0,0 +1,19 @@
+import LengthConverter._
+import org.scalatest.{FlatSpec, Matchers}
+
+class TestLengthConverter extends FlatSpec with Matchers {
+
+  "To and from meters" should "be converted corretly" in {
+    (1 m to m) should be(1)
+    (100 m to km) should be(0.1)
+    (1 ft to m) should be(0.30479 +- 0.00001)
+    (1 yd to m) should be(0.9144 +- 0.0001)
+    (1 mi to m) should be(1609.34 +- 0.01)
+    (1 m to in) should be(39.3700 +- 0.0001)
+  }
+
+  it should "also work for other conversions" in {
+    (1 km to mi) should be(0.621 +- 0.001)
+    (1 in to ft) should be(0.0833 +- 0.0001)
+  }
+}
